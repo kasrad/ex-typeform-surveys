f2a5fde844ad2a21fe1eb8189bee66817d8db588
Radim Kašpárek <radim248@gmail.com>
2018-08-28 14:33:00 +0100

    since parameter altered; if added
    since - possible to denote how many days should be retrieved
    if - now works for no new responses as well

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from keboola import docker
 cfg = docker.Config('/data/')
 token = cfg.get_parameters()['#token']
 form_id = cfg.get_parameters()['form_id']
+dayspan = cfg.get_parameters()['dayspan']
 
 #the timestamp for since should be provided in ISO 8601 format, UTC time
 #e.g. '2017-08-09T00:00:00'  August 9, 2017 at 12:00 a.m. UTC
-since = cfg.get_parameters()['since']
-
+since = (datetime.utcnow() - timedelta(days = dayspan))\
+        .isoformat()
 
 headers = {'Authorization': 'bearer %s' % token}
 params = {'since': since
@@ -22,42 +23,49 @@ params = {'since': since
 #url and the request
 url = 'https://api.typeform.com/forms/' + form_id + '/responses'
 resp = requests.get(url=url, params=params, headers=headers)
-print('The number of new responses is: ' + str(len(resp.json()['items'])))
+no_n_responses = len(resp.json()['items'])
+print('The number of new responses is: ' + str(no_n_responses))
 
 #flattening the response
 results_df = pd.DataFrame(np.zeros((0, 0)))
-for i in range(len(resp.json()['items'])):
-    print('Retrieving response #' + str(i))
-    answers = json_normalize(resp.json()['items'][i]['answers'])\
-        .loc[:, ['choices.labels', 'email', 'number',
-                 'text', 'boolean', 'field.id']]\
-        .fillna('')
+if no_n_responses > 0:
+    for i in range(len(resp.json()['items'])):
+        print('Retrieving response #' + str(i))
+        answers = json_normalize(resp.json()['items'][i]['answers'])\
+            .loc[:, ['choices.labels', 'email', 'number',
+                    'text', 'boolean', 'field.id']]\
+            .fillna('')
 
-    answers['choices.labels'] = answers['choices.labels']\
-        .astype(str)
+        answers['choices.labels'] = answers['choices.labels']\
+            .astype(str)
 
-    answers['number'] = answers['number']\
-        .astype(str)
-    answers['boolean'] = answers['boolean']\
-        .astype(str)
+        answers['number'] = answers['number']\
+            .astype(str)
+        answers['boolean'] = answers['boolean']\
+            .astype(str)
 
-    answers['ans_concat'] = answers[['text', 'email', 'number',
-                                     'choices.labels', 'boolean']]\
-        .apply(lambda x: ''.join(x), axis=1)
+        answers['ans_concat'] = answers[['text', 'email', 'number',
+                                        'choices.labels', 'boolean']]\
+            .apply(lambda x: ''.join(x), axis=1)
 
-    results_df_tmp = answers[['field.id', 'ans_concat']]
+        results_df_tmp = answers[['field.id', 'ans_concat']]
 
-    results_df_tmp = results_df_tmp.set_index('field.id').T
+        results_df_tmp = results_df_tmp.set_index('field.id').T
 
-    if results_df.empty:
-        results_df = results_df_tmp
+        if results_df.empty:
+            results_df = results_df_tmp
+        else:
+            results_df = results_df.append(results_df_tmp)
+
+        print('Response #%s retrieved' % str(i))
+
+    results_df.to_csv('/data/out/tables/answers_applicants.csv', index=False)
+    
     else:
-        results_df = results_df.append(results_df_tmp)
-
-    print('Response #%s retrieved' % str(i))
+        print('No new responses to fetch.')
 
 
 
 
 #write the results
-results_df.to_csv('/data/out/tables/answers_applicants.csv', index=False)
+
