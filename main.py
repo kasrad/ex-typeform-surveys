2f133e195051ef79ee47c632b0f24d142fa92ef7
Radim Kašpárek <radim248@gmail.com>
2018-10-04 08:51:56 +0100

    style corrections

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+"__author__ = 'Radim Kasparek kasrad'"
+"__credits__ = 'Keboola Drak"
+"__component__ = 'Typeform Extractor'"
+
+"""
+Python 3 environment 
+"""
+
+
 import pandas as pd
 import requests
 from pandas.io.json import json_normalize
 import numpy as np
 from keboola import docker
 from datetime import datetime, timedelta
+import logging
 
+# Logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt="%Y-%m-%d %H:%M:%S")
 
 cfg = docker.Config('/data/')
 token = cfg.get_parameters()['#token']
@@ -18,40 +33,40 @@ params = {'since': since
           }
 
 
-#url and the request
+# url and the request
 url = 'https://api.typeform.com/forms/' + form_id + '/responses'
 resp = requests.get(url=url, params=params, headers=headers)
 no_n_responses = len(resp.json()['items'])
-print('The number of new responses is: ' + str(no_n_responses))
+logging.info('The number of new responses is: ' + str(no_n_responses))
 
-#flattening the response
+# flattening the response
 results_df = pd.DataFrame(np.zeros((0, 0)))
 if no_n_responses > 0:
     for i in range(len(resp.json()['items'])):
-        print('Retrieving response #' + str(i))
-        #id of the applicant + time_submitted
+        logging.info('Retrieving response #' + str(i))
+        # id of the applicant + time_submitted
         id_applicant = resp.json()['items'][i]['metadata']['referer']
         time_submitted = resp.json()['items'][i]['submitted_at']
 
-        #create df from flattened json
+        # create df from flattened json
         response_df = json_normalize(resp.json()['items'][i]['answers'])
         resp_df_cols = response_df.columns
 
-        #all the columns that could be of interest
+        # all the columns that could be of interest
         all_cols_needed = ['choices.labels', 'email', 'number', 'text',
-                             'boolean', 'field.id', 'url']
+                           'boolean', 'field.id', 'url']
 
-        #add the columns to those responses that do not contain them
+        # add the columns to those responses that do not contain them
         for col_name in resp_df_cols:
             response_df[col_name] = '' if (col_name not in all_cols_needed) \
-                                        else response_df[col_name]
+                                    else response_df[col_name]
         
-        #take only the columns of interest, switch NaNs with '' for later concat
+        # take only the columns of interest, switch NaNs with '' for later concat
         answers = response_df\
             .loc[:, all_cols_needed]\
             .fillna('')
 
-        #cast the columns as strings
+        # cast the columns as strings
         answers['choices.labels'] = answers['choices.labels']\
             .astype(str)
         answers['number'] = answers['number']\
@@ -59,34 +74,28 @@ if no_n_responses > 0:
         answers['boolean'] = answers['boolean']\
             .astype(str)
 
-        #concat the values
+        # concat the values
         answers['ans_concat'] = answers[['text', 'email', 'number',
-                                        'choices.labels', 'boolean',
-                                        'url']]\
+                                         'choices.labels', 'boolean',
+                                         'url']]\
             .apply(lambda x: ''.join(x), axis=1)
 
-        #id of the question + value
+        # id of the question + value
         results_df_tmp = answers[['field.id', 'ans_concat']]
         results_df_tmp = results_df_tmp.set_index('field.id').T
         results_df_tmp['id'] = [id_applicant]
         results_df_tmp['time_submitted'] = [time_submitted]
 
-        #in the first run create the df, in the following just append
+        # in the first run create the df, in the following just append
         if results_df.empty:
             results_df = results_df_tmp
         else:
             results_df = results_df.append(results_df_tmp)
 
-        print('Response #%s retrieved' % str(i))
+        logging.info('Response #%s retrieved' % str(i))
 
-    #write the results
+    # write the results
     results_df.to_csv('/data/out/tables/answers_applicants.csv', index=False)
     
 else:
-    print('No new responses to fetch.')
-
-
-
-
-
-
+    logging.info('No new responses to fetch.')
